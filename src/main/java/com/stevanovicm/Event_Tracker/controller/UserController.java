a4f5a96ae53df3233df0cbb08c4cd3d743ac0eda
Milos Stevanovic <stevanovicmilos0003@gmail.com>
2025-07-14 23:26:10 +0200

    Manje promene u nacinu dobijanja korisnika i dodato ignorisanje objekta korisnik kad se vraca event

--- src/main/java/com/stevanovicm/Event_Tracker/controller/UserController.java
+++ src/main/java/com/stevanovicm/Event_Tracker/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.stevanovicm.Event_Tracker.controller;
 
 import com.stevanovicm.Event_Tracker.dto.UserDetailsResponse;
+import com.stevanovicm.Event_Tracker.entity.User.User;
 import com.stevanovicm.Event_Tracker.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,7 +16,7 @@ public class UserController {
 
   //na adresi /api/UserDetails poyivamo servis koji vadi sve podatke o trenutnom korisniku i vraca u frmatu naseg dto recorda UserDetailsResponse
   @GetMapping
-  public ResponseEntity<UserDetailsResponse> getCurrentUserDetails() {
-    return ResponseEntity.ok((userService.getCurrentUserDetails()));
+  public ResponseEntity<UserDetailsResponse> getCurrentUserDetails(@AuthenticationPrincipal User user) {
+    return ResponseEntity.ok((userService.getCurrentUserDetails(user)));
   }
 }
--- src/main/java/com/stevanovicm/Event_Tracker/service/UserService.java
+++ src/main/java/com/stevanovicm/Event_Tracker/service/UserService.java
@@ -2,26 +2,13 @@ package com.stevanovicm.Event_Tracker.service;
 
 import com.stevanovicm.Event_Tracker.dto.UserDetailsResponse;
 import com.stevanovicm.Event_Tracker.entity.User.User;
-import com.stevanovicm.Event_Tracker.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class UserService {
-    private final UserRepository userRepository;
 
     //funkcija koja vraca u nasme zahtevanom formatu podatke o korisniku
-    public UserDetailsResponse getCurrentUserDetails() {
-      //izvlacimo username korisnika preko spring sekjutritija(promeniti metodu standardizovati sa ostalim da ide prkeo ida-a i laks eje da se prosledi direkt iz kontrolera)
-        String username = SecurityContextHolder.getContext().getAuthentication().getName();
-
-        //vadimo korisnika dodati
-        User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("Korisnik nije pronadjen"));
-
+    public UserDetailsResponse getCurrentUserDetails(User user) {
         //Vracamo korisnika tacno onako kao record zahteva samo ta polja
         return new UserDetailsResponse(
                 user.getUsername(),
